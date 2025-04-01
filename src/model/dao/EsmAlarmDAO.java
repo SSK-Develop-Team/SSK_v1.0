3a10e1c3f1c46fb820c2fedff4add213142e2762
na0young <0na021126@gmail.com>
2025-04-01 20:05:08 +0900

    [Feat] #101 DB 연결 및 알람 DAO 메서드 추가
    - DBConnection: getConnection 메서드 추가
    - EsmAlarmDAO: 새로운 알람 관련 메서드 추가

--- src/model/dao/EsmAlarmDAO.java
+++ src/model/dao/EsmAlarmDAO.java
@@ -5,27 +5,29 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import model.dto.EsmAlarm;
 import model.dto.User;
 
 public class EsmAlarmDAO {
 	private final static String SQLST_SELECT_ESMALARM_BY_LOGIN_ID = "select * from esm_alarm where user_login_id = ?";
-	//사용자의 Esm 알람 정보 불러오기 (Connection con, int userId)
+
+	// 사용자의 Esm 알람 정보 불러오기 (Connection con, int userId)
 	public static ArrayList<EsmAlarm> getEsmAlarmListByUser(Connection con, int userId) {
 		try {
 			PreparedStatement pstmt = con.prepareStatement("select * from esm_alarm where user_id= ?");
 			pstmt.setInt(1, userId);
 			ResultSet rs = pstmt.executeQuery();
 			ArrayList<EsmAlarm> alarmList = new ArrayList<EsmAlarm>();
-			while(rs.next()) { 
-				EsmAlarm esmTime= new EsmAlarm();
+			while (rs.next()) {
+				EsmAlarm esmTime = new EsmAlarm();
 				esmTime.setAlarmId(rs.getInt(1));
 				esmTime.setAlarmStart(rs.getTime(2));
 				esmTime.setAlarmEnd(rs.getTime(3));
 				esmTime.setAlarmInterval(rs.getInt(4));
 				esmTime.setUserId(rs.getInt(5));
-				
+
 				alarmList.add(esmTime);
 			}
 			return alarmList;
@@ -34,43 +36,83 @@ public class EsmAlarmDAO {
 		}
 		return null;
 	}
-	
-	//사용자의 ESm 알람 정보 저장하기 (Connection con, int userId)
-	public static boolean insertUserAlarm(Connection con, EsmAlarm esmAlarm ){
+
+	// 사용자의 ESm 알람 정보 저장하기 (Connection con, int userId)
+	public static boolean insertUserAlarm(Connection con, EsmAlarm esmAlarm) {
 		try {
-			PreparedStatement pstmt = con.prepareStatement("insert esm_alarm(start_time, end_time, interval_time,user_id) values(?,?,?,?)");
+			PreparedStatement pstmt = con
+					.prepareStatement("insert esm_alarm(start_time, end_time, interval_time,user_id) values(?,?,?,?)");
 			pstmt.setTime(1, esmAlarm.getAlarmStart());
 			pstmt.setTime(2, esmAlarm.getAlarmEnd());
 			pstmt.setInt(3, esmAlarm.getAlarmInterval());
 			pstmt.setInt(4, esmAlarm.getUserId());
 
 			int insertCount = pstmt.executeUpdate();
-			if(insertCount >= 1) {
+			if (insertCount >= 1) {
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
-	//사용자의 ESm 알람 정보 삭제하기 (Connection con, int userId) //하나씩만 삭제하게 코드수정하기
+
+	// 사용자의 ESm 알람 정보 삭제하기 (Connection con, int userId) //하나씩만 삭제하게 코드수정하기
 	public static void deleteUserAlarm(Connection con, int userId) {
 		try {
 			PreparedStatement pstmt = con.prepareStatement("DELETE FROM esm_alarm WHERE user_id = ?");
 			pstmt.setInt(1, userId);
-			
+
 			pstmt.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-}
 
+	// 모든 알람 시간 받아오기
+	public static List<EsmAlarm> getAllAlarms(Connection con) {
+		List<EsmAlarm> alarmList = new ArrayList<>();
+		String sql = "SELECT user_id, start_time, end_time, interval_time FROM esm_alarm";
+
+		try (PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
+			while (rs.next()) {
+				EsmAlarm alarm = new EsmAlarm();
+				alarm.setUserId(rs.getInt("user_id")); // userId 설정
+				alarm.setAlarmStart(rs.getTime("start_time"));
+				alarm.setAlarmEnd(rs.getTime("end_time"));
+				alarm.setAlarmInterval(rs.getInt("interval_time"));
+				alarmList.add(alarm);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return alarmList;
+	}
+
+	// 현재 시간에 해당하는 사용자 조회하기
+	public static List<Integer> getMatchingUsers(Connection con, String alarmTime) throws SQLException {
+		String sql = "SELECT user_id " +
+				"FROM esm_alarm " +
+				"WHERE TIME(?) BETWEEN start_time AND end_time " +
+				"  AND MOD(TIMESTAMPDIFF(MINUTE, start_time, TIME(?)) / 60, interval_time) = 0";
+		List<Integer> userIds = new ArrayList<>();
+
+		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setString(1, alarmTime);
+			pstmt.setString(2, alarmTime);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs.next()) {
+					userIds.add(rs.getInt("user_id"));
+				}
+			}
+		}
+
+		return userIds;
+	}
+
+}
--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -13,73 +13,80 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
 @WebListener
 public class DBConnection implements ServletContextListener {
+	private static Connection conn = null; // DB연결 객체 저장
 
-    public DBConnection() {
-    }
+	public DBConnection() {
+	}
 
-    public void contextDestroyed(ServletContextEvent sce)  { 
-    	Connection conn = (Connection) sce.getServletContext().getAttribute("DBconnection");
-    	try {
-    		conn.close();
-    	} 
-    	catch (SQLException e) {
-    		e.printStackTrace();
-    	}
-    }
+	public void contextDestroyed(ServletContextEvent sce) {
+		Connection conn = (Connection) sce.getServletContext().getAttribute("DBconnection");
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// Quartz 스케줄러 종료 추가
+		try {
+			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+			if (scheduler != null && scheduler.isStarted()) {
+				scheduler.shutdown(); // 스케줄러 종료
+				System.out.println("Quartz 스케줄러가 종료되었습니다.");
+			}
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void contextInitialized(ServletContextEvent sce)  { 
-    	Connection conn = null;
-    	/*
-    	Properties connectionProps = new Properties();
-    	ServletContext sc = sce.getServletContext();
-    	
-    	String DBUrl = sc.getInitParameter("JDBCUrl");
-    	String DBuser = sc.getInitParameter("DBuser");
-    	String DBpasswd = sc.getInitParameter("DBpasswd");
-    	String DBTimeZone = sc.getInitParameter("DBTimeZone");
-    	
-    	connectionProps.put("user", DBuser);
-    	connectionProps.put("password", DBpasswd);
-    	connectionProps.put("serverTimezone", DBTimeZone);
-    	
-    	try {
-	    	conn = DriverManager.getConnection(DBUrl, connectionProps);
-    	}  
-    	catch (SQLException e) {
-    		e.printStackTrace();
-    	}
-    	
-    	sc.setAttribute("DBconnection", conn);
-    */
-    	ServletContext context = sce.getServletContext();
-    	Context initContext;
-    	DataSource dataSource = null;
-    	// DBCP 연결
-    	try {
-	    	initContext = new InitialContext();
-	    	// JNDI 이용을 위한 객체 생성
-	    	Context envContext = (Context)initContext.lookup("java:comp/env");
-	    	// lookup() 등록된 naming서비스로부터 자원을 찾고자 할 때 사용하는 메소드
-	    	// context 객체를 통해 이름으로 Resource 획득
-	    	dataSource = (DataSource)envContext.lookup("jdbc/SSK");
-	    	System.out.println("Load dbcp Driver");
+	public void contextInitialized(ServletContextEvent sce) {
+		// Connection conn = null;
+		ServletContext context = sce.getServletContext();
+		Context initContext;
+		DataSource dataSource = null;
+		// DBCP 연결
+		try {
+			initContext = new InitialContext();
+			// JNDI 이용을 위한 객체 생성
+			Context envContext = (Context) initContext.lookup("java:comp/env");
+			// lookup() 등록된 naming서비스로부터 자원을 찾고자 할 때 사용하는 메소드
+			// context 객체를 통해 이름으로 Resource 획득
+			dataSource = (DataSource) envContext.lookup("jdbc/SSK");
+			System.out.println("Load dbcp Driver");
 
-    	} catch (NamingException e1) {
-    		e1.printStackTrace();
-    	}
-    	
-    	try {
-    		conn = dataSource.getConnection();
-    		if(conn != null) {
-	    		context.setAttribute("DBconnection", conn);
-	    		System.out.println("Succeed DB connection");
-    		}
-    		} catch (SQLException e) {
-	    		e.printStackTrace();
-	    		System.out.println("Fail DB connection");
-    		}
-    }
-	
+		} catch (NamingException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			conn = dataSource.getConnection();
+			if (conn != null) {
+				context.setAttribute("DBconnection", conn);
+				System.out.println("Succeed DB connection");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Fail DB connection");
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		// pool에 사용한 커넥션 있다면 반환, 없으면 새로운 커넥션 반환
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:comp/env");
+			DataSource dataSource = (DataSource) envContext.lookup("jdbc/SSK");
+			Connection conn = dataSource.getConnection();
+
+			return conn;
+		} catch (NamingException | SQLException e) {
+			System.err.println("DB 연결 실패: " + e.getMessage());
+			throw new SQLException("DB 연결 실패", e);
+		}
+	}
+
 }
